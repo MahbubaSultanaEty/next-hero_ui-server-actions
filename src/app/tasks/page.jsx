301151fe24d3d80e0195ce1ba61fd1ad04d55eee
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-04-22 20:52:12 +0600

    44-2 Using HeroUI components with fetched data (cards, lists)

---
+++ src/app/tasks/page.jsx
@@ -0,0 +1,17 @@
+import TaskCard from "@/components/TaskCard";
+import { getTasks } from "@/lib/tasks";
+
+const TasksPage =async () => {
+    const tasks = await getTasks();
+    console.log(tasks);
+    return (
+        <div>
+            <h2 className="text-2xl text-center">TASKS : {tasks.length}</h2>
+            <div className="grid md:grid-cols-2 gap-4">
+                {tasks.map(task => <TaskCard key={task.id} task={task}></TaskCard>)}
+            </div>
+        </div>
+    );
+};
+
+export default TasksPage;
---
+++ src/components/TaskCard.jsx
@@ -0,0 +1,31 @@
+import { Card, Link } from "@heroui/react";
+
+
+
+const TaskCard = ({ task }) => { 
+    const {title, description, status } = task;
+    return (
+    <Card variant="outline" className="w-[400px] bg-blue-950 mx-auto">
+    {/*    <CircleDollar aria-label="Dollar sign icon" className="text-primary size-6" role="img" /> */}
+      <Card.Header>
+                <Card.Title>{title}!</Card.Title>
+        <Card.Description>
+          {description}
+        </Card.Description>
+      </Card.Header>
+      <Card.Footer>
+        <Link
+          aria-label="Go to Acme Creator Hub (opens in new tab)"
+          href="https://heroui.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Creator Hub
+          <Link.Icon aria-hidden="true" />
+        </Link>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default TaskCard;
